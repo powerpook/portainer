a8aa09723ce4f0ffc8fc5480ee3a627b8f31edb7
Sven Dowideit <sven.dowideit@portainer.io>
2021-10-20 21:17:07 +1000

    yay, now bolt is only mentioned inside the api/database/ dir
    Signed-off-by: Sven Dowideit <sven.dowideit@portainer.io>

--- api/dataservices/interface.go
+++ api/dataservices/interface.go
@@ -3,6 +3,7 @@ package dataservices
 // 	"github.com/portainer/portainer/api/dataservices"
 
 import (
+	"github.com/portainer/portainer/api/dataservices/errors"
 	"io"
 
 	portainer "github.com/portainer/portainer/api"
@@ -261,3 +262,7 @@ type (
 		BucketName() string
 	}
 )
+
+func IsErrObjectNotFound(e error) bool {
+	return e == errors.ErrObjectNotFound
+}
