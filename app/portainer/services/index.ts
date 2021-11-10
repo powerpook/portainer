e79124289525757f141b4d534d2b0c70d12a584f
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2021-11-10 17:04:53 +0200

    refactor(app): remove angular from http-request [EE-1899] (#981)

---
+++ app/portainer/services/index.ts
@@ -0,0 +1,7 @@
+import angular from 'angular';
+
+import { HttpRequestHelperAngular } from './http-request.helper';
+
+export default angular
+  .module('portainer.app.services', [])
+  .factory('HttpRequestHelper', HttpRequestHelperAngular).name;
