7fd057ae7f8ebbdae5323cc65b79c8747c3b28ea
Chaim Lev-Ari <chiptus@gmail.com>
2021-11-14 11:56:30 +0200

    feat(app): add axios

--- app/config.js
+++ app/config.js
@@ -1,6 +1,7 @@
 import toastr from 'toastr';
 import { Terminal } from 'xterm';
 import * as fit from 'xterm/lib/addons/fit/fit';
+import { agentInterceptor } from './portainer/services/axios';
 
 angular.module('portainer').config([
   '$urlRouterProvider',
@@ -33,22 +34,11 @@ angular.module('portainer').config([
     $httpProvider.defaults.headers.put['Content-Type'] = 'application/json';
     $httpProvider.defaults.headers.patch['Content-Type'] = 'application/json';
 
-    $httpProvider.interceptors.push([
-      'HttpRequestHelper',
-      function (HttpRequestHelper) {
-        return {
-          request: function (config) {
-            if (config.url.indexOf('/docker/') > -1) {
-              config.headers['X-PortainerAgent-Target'] = HttpRequestHelper.portainerAgentTargetHeader();
-              if (HttpRequestHelper.portainerAgentManagerOperation()) {
-                config.headers['X-PortainerAgent-ManagerOperation'] = '1';
-              }
-            }
-            return config;
-          },
-        };
-      },
-    ]);
+    $httpProvider.interceptors.push(function () {
+      return {
+        request: agentInterceptor,
+      };
+    });
 
     toastr.options = {
       timeOut: 3000,
